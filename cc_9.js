5541becea301c24f68255dc4705afa389bbbe5c8
mallikanicole <malmichael23@gmail.com>
2025-02-22 11:02:07 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -43,3 +43,25 @@ console.log(mgr1.getDetails());
 console.log(mgr1.calculateBonus()); 
 // Expected output: 9600
 
+//Task 3 Creating a Company Class
+
+class Company {
+    constructor(name) {
+        this.name=name;
+        this.employees=[];
+    }//add employee array
+    addEmployee(employee){
+        this.employees.push(employee);
+    }//add in employees to array
+    listEmployees(){
+        return this.employees.forEach(employee=>console.log(employee.getDetails()));
+    }//log employee details
+};//create company class
+
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
